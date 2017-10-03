a71e7ac5fd6225d07261266b9edc4b413c71b424
wisamasd <skay-91@mail.ru>
2017-10-03 22:08:36 +0300

    Defines correction accordace with PCB in/out smd-gamepad pin

--- defines.h
+++ defines.h
@@ -1,21 +1,21 @@
 #define SMD_ENABLED
 #define SMD_SECOND_ENABLED
-#define SMD_SELECT_PORT D
-#define SMD_SELECT_PIN 6
+#define SMD_SELECT_PORT B
+#define SMD_SELECT_PIN 0 //7
 #define SMD_DATA_PORT D
-#define SMD_DATA0_PIN 0
-#define SMD_DATA1_PIN 1
-#define SMD_DATA2_PIN 2
-#define SMD_DATA3_PIN 3
-#define SMD_DATA4_PIN 4
-#define SMD_DATA5_PIN 5
+#define SMD_DATA0_PIN 2  //1.1
+#define SMD_DATA1_PIN 3  //1.2
+#define SMD_DATA2_PIN 4  //1.3
+#define SMD_DATA3_PIN 5  //1.4
+#define SMD_DATA4_PIN 1  //1.6
+#define SMD_DATA5_PIN 6  //1.9
 #define SMD_DATA_PORT2 B
-#define SMD_DATA0_PIN2 0
-#define SMD_DATA1_PIN2 1
-#define SMD_DATA2_PIN2 2
-#define SMD_DATA3_PIN2 3
-#define SMD_DATA4_PIN2 4
-#define SMD_DATA5_PIN2 5
+#define SMD_DATA0_PIN2 4  //2.1
+#define SMD_DATA1_PIN2 5  //2.2
+#define SMD_DATA2_PIN2 6  //2.3
+#define SMD_DATA3_PIN2 7  //2.4
+#define SMD_DATA4_PIN2 2  //2.6
+#define SMD_DATA5_PIN2 3  //2.9
 #define SMD_USE_DENDY9_PIN
 
 #define NES_ENABLED
